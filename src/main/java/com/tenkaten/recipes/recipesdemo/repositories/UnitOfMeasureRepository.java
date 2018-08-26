49e0b5562da6c3e9281e827a654e9c02d9bf7c15
Koen <koentk84@gmail.com>
2018-08-26 19:26:07 +0200

    add method to repo, add sql script

--- src/main/java/com/tenkaten/recipes/recipesdemo/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/tenkaten/recipes/recipesdemo/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.tenkaten.recipes.recipesdemo.repositories;
 import com.tenkaten.recipes.recipesdemo.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
