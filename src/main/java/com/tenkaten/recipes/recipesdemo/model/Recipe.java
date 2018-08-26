b95b173acb8cc8060b7f30b735dcaee0edcb90c4
Koen <koentk84@gmail.com>
2018-08-26 16:32:26 +0200

    add ingredient pojo

--- src/main/java/com/tenkaten/recipes/recipesdemo/model/Recipe.java
+++ src/main/java/com/tenkaten/recipes/recipesdemo/model/Recipe.java
@@ -1,6 +1,8 @@
 package com.tenkaten.recipes.recipesdemo.model;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -22,6 +24,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -101,5 +106,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
